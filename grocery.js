de9f901009bac4f9fbc26a7dd126c52c06ff9b8d
Jenny Allar <jennyallar@gmail.com>
2016-02-17 18:09:32 -0700

    bare bones new directions

--- grocery.js
+++ grocery.js
@@ -8,14 +8,21 @@ $(document).ready(function() {
   {name: "Jalapeno", status: "complete", price: ".15", quantity: 2}
   ];
 
-//1. Use the inputs and add button to add grocery items to the list. Default status should be "needed"
 
-//2. Add groceries to <ul>. Display the item, price, quantity, and total.
+// Before we start anything, string up the css file, this javascript file, and
+// the jQuery CDN to grocery.html file.
 
+//1. Display the existing list of grocery items (from the grocery array)
+// in an unordered list in the "list" id that already exists in grocery.html.
+// Display each item's name, price, and quantity.
+// Ex: Tomatoes (5) @ $3.99
 
+//2. Use the inputs and add button to add grocery items to the beginning of the list.
+// Default status should be "needed". The item should appear above the existing grocery items.
+
+//3. Make sure that the grocery list displayed updates when you add an item to the list.
 
 //3. Display the total cost of the groceries. Make sure this updates as you add items to the list.
-// function totalCost(){
 
 //4. Put a check in to make sure users aren't adding items without a name, price, or quatity.
 
